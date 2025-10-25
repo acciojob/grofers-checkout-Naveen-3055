d2909c9847e251c22523f10c572213e52eceef7c
Naveen Kumar <knaveenk869@gmail.com>
2025-10-25 13:13:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
+const prices = document.querySelectorAll('.price');
+let sum=0;
 const getSum = () => {
 //Add your code here
-  
+	prices.forEach((p)=>{
+		sum += parseFloat(p.textContent)
+	})
+	  const totalRow = document.createElement('tr');
+	const totalCell = document.createElement('td');
+	totalCell.colSpan = 2;
+	totalCell.textContent = `Total is ${sum}`
+	totalRow.appendChild(totalCell)
+	document.getElementById('grocery').appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
